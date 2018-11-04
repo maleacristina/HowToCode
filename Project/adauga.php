d776a3262a405f7c181f8733c31379d6094fd4a2
maleacristina <maleacristinaecaterina@gmail.com>
2018-11-04 21:14:34 +0200

    Interface for add products and link for database

---
+++ Project/adauga.php
@@ -0,0 +1,19 @@
+<?php
+$nume=$_POST['nume'];
+$pret=$_POST['pret'];
+$categorie=$_POST['categorie'];
+
+echo "Am adaugat produsul cu numele: " .$nume . "</br>";
+echo "Pretul produsului este: " . $pret . "</br>";
+echo "Face parte din categoria: " . $categorie . "</br>";
+
+
+
+
+
+
+
+
+
+
+?>
